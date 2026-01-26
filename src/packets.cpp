e9762420d559a2f219c0a1743b850be16c10d4af
FOX <ba9309066@gmail.com>
2026-01-26 05:29:51 +0200

    Reformat the file -i deleted the class struct-

--- src/packets.cpp
+++ src/packets.cpp
@@ -4,78 +4,42 @@
 #include <random>
 using namespace std;
 
-class Packets {
-private:
-	string ip_source, ip_destination;
-	string protocol;
-	int port;
-	random_device rd;
-	mt19937 gen;
-public:
-	Packets () : gen(rd()) {};
-	string get_ip_source() {
-		ip_source = Generate_IP();
-		return ip_source;
-	}
-	string get_ip_destination() {
-		ip_destination = Generate_IP();
-		return ip_destination;
-	}
-	string Generate_IP() {	
-		uniform_int_distribution<> dist(1, 254);
-		int RandomNumber = dist(gen);
-		string ip;
-		ip = "192.168.1." + to_string(RandomNumber);
-		return ip;
-	}
-	int Generate_PORT() {
-		uniform_int_distribution<> dist(1, 9999);
-		int RandomNumber = dist(gen);
-		port = RandomNumber;
-		return port;
-	}
-	string Generate_PROTOCOL() {	
-		uniform_int_distribution<> dist(1, 7);
-		int RandomNumber = dist(gen);
-		if (RandomNumber == 1) {
-			protocol = "HTTP";
-			port = 80;
-			return protocol;
-		}
-		else if (RandomNumber == 2) {
-			protocol = "HTTPS";
-			port = 443;
-			return protocol;
-		}
-		else if (RandomNumber == 3) {
-			protocol = "FTP";
-			port = 21;
-			return protocol;
-		}
-		else if (RandomNumber == 4) {
-			protocol = "SSH";
-			port = 22;
-			return protocol;
-		}
-		else if (RandomNumber == 5) {
-			protocol = "Telnet";
-			port = 23;
-			return protocol;
-		}
-		else if (RandomNumber == 6) {
-			protocol = "SMTP";
-			port = 25;
-			return protocol;
-		}
-		else if (RandomNumber == 7) {
-			protocol = "DNS";
-			port = 53;
-			return protocol;
-		}
-		else {
-			protocol = "ICMP";
-			port = 0;
-			return protocol;
-		}
-	}
-};
+Packets::Packets() : gen(rd()) {}
+
+string Packets::get_ip_source() {
+    ip_source = Generate_IP();
+    return ip_source;
+}
+
+string Packets::get_ip_destination() {
+    ip_destination = Generate_IP();
+    return ip_destination;
+}
+
+string Packets::Generate_IP() {
+    uniform_int_distribution<> dist(1, 254);
+    int RandomNumber = dist(gen);
+    string ip = "192.168.1." + to_string(RandomNumber);
+    return ip;
+}
+
+int Packets::Generate_PORT() {
+    uniform_int_distribution<> dist(1, 9999);
+    int RandomNumber = dist(gen);
+    port = RandomNumber;
+    return port;
+}
+
+string Packets::Generate_PROTOCOL() {
+    uniform_int_distribution<> dist(1, 7);
+    int RandomNumber = dist(gen);
+    if (RandomNumber == 1) { protocol = "HTTP"; port = 80; }
+    else if (RandomNumber == 2) { protocol = "HTTPS"; port = 443; }
+    else if (RandomNumber == 3) { protocol = "FTP"; port = 21; }
+    else if (RandomNumber == 4) { protocol = "SSH"; port = 22; }
+    else if (RandomNumber == 5) { protocol = "Telnet"; port = 23; }
+    else if (RandomNumber == 6) { protocol = "SMTP"; port = 25; }
+    else if (RandomNumber == 7) { protocol = "DNS"; port = 53; }
+    else { protocol = "ICMP"; port = 0; }
+    return protocol;
+}
