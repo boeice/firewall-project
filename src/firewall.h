1c5da7e29056262eb2d93469fdaccf325e3b64a2
FOX <ba9309066@gmail.com>
2026-01-31 08:31:25 +0200

    finally firewall is done

--- src/firewall.h
+++ src/firewall.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 class Firewall {
@@ -8,7 +9,11 @@ private:
 	map<string, string> ip_rules;
 	map<int, string> port_rules;
 	map<string, string> protocol_rules;
+	vector<Packets> packetsList;
+	vector<string> result; // action and reason
+	//pair<string, string> checkResult;
 public:
 	Firewall();
-	string BLOCKLIST();
+	void CHECK_PACKETS();
+	void PACKETS_LIST();
 };
